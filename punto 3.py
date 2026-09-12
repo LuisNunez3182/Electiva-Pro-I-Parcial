88ae984dcbdc1c6b54e7156c9f4d1012b5c2415e
Xavier835 <Slathersaufuen@gmail.com>
2026-09-11 19:41:23 -0500

    Update and rename 3.py to punto 3.py

--- punto 3.py
+++ punto 3.py
@@ -1,11 +1,8 @@
 texto_ingresado = input("Ingresa varias palabras separadas por espacios: ")
-
 lista_palabras = texto_ingresado.split()
-
 palabras_largas = []
 
 for palabra in lista_palabras:
-    
     if len(palabra) > 5:
         palabras_largas.append(palabra)
 

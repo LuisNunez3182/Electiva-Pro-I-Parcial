b80439b2b7dbb0664f29394c42ef738ba195c05e
Camilox64 <camilofabrega12@gmail.com>
2026-09-14 16:54:05 -0500

    Implement input validation for string input
    Added input validation to ensure no numbers are included in the string.

--- punto2.py
+++ punto2.py
@@ -1,10 +1,14 @@
 cadena = input("Ingrese una cadena: ")
 
+while not cadena.replace(" ", "").isalpha():
+    print("No se permiten números.")
+    cadena = input("Ingrese una cadena: ")
+
 caracteres = 0
 vocales = 0
 consonantes = 0
 
-for caracter in cadena:
+for caracter in cadena.lower():
     caracteres = caracteres + 1
 
     if caracter == "a" or caracter == "e" or caracter == "i" or caracter == "o" or caracter == "u":
